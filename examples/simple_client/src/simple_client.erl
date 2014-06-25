979d190828949e7d9f210fc83583ee30f0a2da82
Bas Wegh <BasWegh@gmx.de>
2014-06-25 07:50:15 +0200

    Add more output in simple client

--- examples/simple_client/src/simple_client.erl
+++ examples/simple_client/src/simple_client.erl
@@ -32,11 +32,16 @@ start_link() ->
   gen_server:start_link(?MODULE, [], []).
 
 init(_) ->
-  io:format("starting client~n"),
+  io:format("starting client ... "),
   {ok,Con} = erwa:start_client(),
+  io:format("done.~nconnecting to realm ~p at ~p:~p ... ",[?REALM,?HOST,?PORT]),
   {ok,SessionId,_RouterDetails} = erwa:connect(Con,?HOST,?PORT,?REALM,?ENCODING),
+  io:format("done (~p).~nsubscribe to ~p ... ",[SessionId,?EVENT_URL]),
   {ok,SubId} = erwa:subscribe(Con,[{}],?EVENT_URL),
+  io:format("subscribed (~p).~nregister ~p ... ",[SubId,?RPC_ECHO_URL]),
   {ok,EchoRPCId} = erwa:register(Con,[{}],?RPC_ECHO_URL),
+  io:format("registered (~p).~nclient sucessfully initialized.~n",[EchoRPCId]),
+  io:format("~nIf you send me an event on ~p I will call the procedure ~p~n",[?EVENT_URL,?RPC_SQUARE_URL]),
   {ok,#state{con=Con,session=SessionId,rpc_echo_id=EchoRPCId,event_sub_id=SubId}}.
 
 handle_call(_,_From,State) ->
@@ -47,23 +52,29 @@ handle_cast(_Msg,State) ->
 
 
 handle_info({erwa,{event,SubId,_PublicationId,_Details,Arguments,ArgumentsKw}},#state{event_sub_id=SubId,con=Con}=State) ->
-  io:format("received event ~p ~p~n",[Arguments,ArgumentsKw]),
-  {ok,Details,ResA,ResAKw} = erwa:call(Con,[{}],?RPC_SQUARE_URL,[7]),
-  [Res] = ResA,
-  io:format("result of call was: ~B ~p [~p]~n",[Res,ResAKw,Details]),
-  Res = 49,
+  io:format("received event ~p ~p on [~p]~n",[Arguments,ArgumentsKw,SubId]),
+  Params = [3],
+  io:format("calling ~p ~p ... ",[?RPC_SQUARE_URL,Params]),
+  {ok,Details,ResA,ResAKw} = erwa:call(Con,[{}],?RPC_SQUARE_URL,Params),
+  io:format("result is: ~p ~p [~p]~n",[ResA,ResAKw,Details]),
+  ResA = [9],
+  io:format("unsubscribing from ~p ... ",[SubId]),
   ok = erwa:unsubscribe(Con,SubId),
+  io:format("unsubscribed.~n"),
   {noreply,State};
 
 
 handle_info({erwa,{invocation,RequestId,RpcId,_Details,Arguments,ArgumentsKw}},#state{rpc_echo_id=RpcId,con=Con}=State) ->
   %invocation of the echo rpc
+  io:format("been called [~p] with params ~p ~p ... will just send them back ...",[RpcId,Arguments,ArgumentsKw]),
   ok = erwa:yield(Con,RequestId,[{}],Arguments,ArgumentsKw),
+  io:format("sent.~nunregistering ~p ... ",[RpcId]),
   ok = erwa:unregister(Con,RpcId),
+  io:format("unregistered.~n"),
   {noreply,State};
 
 handle_info(Msg,State) ->
-  io:format("received message: ~p~n",[Msg]),
+  io:format("~nreceived unknown message: ~p~n",[Msg]),
   {noreply,State}.
 
 terminate(_Reason,_State) ->
