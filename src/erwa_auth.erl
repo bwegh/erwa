021e9f2c58dd3a223a2f89add5e83c1483a15549
Bas Wegh <BasWegh@gmx.de>
2015-01-30 23:23:52 +0100

    Add function to create wampcra challenge.

--- src/erwa_auth.erl
+++ src/erwa_auth.erl
@@ -24,6 +24,7 @@
 
 -export([wamp_cra/2]).
 -export([pbkdf2/4]).
+-export([create_wampcra_challenge/4]).
 
 %% @doc calculates the cryptographic hash of the challenge by using the secret key.
 -spec wamp_cra(Key :: binary(), Challenge :: binary() ) -> binary().
@@ -38,6 +39,21 @@ wamp_cra(Key,Challenge) ->
                       Length :: non_neg_integer()) -> {ok, NewKey :: binary()}.
 pbkdf2(SecretKey, Salt, Iterations, Length) ->
   pbkdf2:pbkdf2(SecretKey, Salt, Iterations, Length).
+
+
+-spec create_wampcra_challenge(AuthProvider :: binary(), AuthId :: binary(), Authrole :: binary(), Session :: non_neg_integer() ) -> {ok,Challenge :: binary(), calendar:timestamp() }.
+create_wampcra_challenge(AuthProvider, AuthId, Authrole, Session) ->
+  Now = erlang:now(),
+  {{Year,Month,Day},{Hour,Minute,Seconds}} = calendar:now_to_universal_time(Now),
+  Timestamp = list_to_binary(io_lib:format("~.10B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0B.000Z",[Year,Month,Day,Hour,Minute,Seconds])),
+  Challenge = jsx:encode([{<<"nonce">>,nonce()},{<<"authprovider">>,AuthProvider},
+                          {<<"authid">>,AuthId},{<<"timestamp">>,Timestamp},
+                          {<<"authrole">>,Authrole},{<<"authmethod">>,<<"wampcra">>},
+                          {<<"session">>,Session}]),
+  {ok, Challenge, Now }.
+
+nonce() ->
+  base64:encode(crypto:strong_rand_bytes(15)).
 
 
 -ifdef(TEST).
